4e293f2023617b8e6c90cb958e9eb0115506f70f
KeerthiKumar03 <keerthigoud0308@gmail.com>
2026-06-23 15:20:14 +0530

    Committin todays's work

---
+++ Day-23-Campus-Core/campucore-backend/src/main/java/com/campuscore/campucore_backend/CampucoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.campuscore.campucore_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampucoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampucoreBackendApplication.class, args);
+	}
+
+}
---
+++ Day-23-Campus-Core/campucore-backend/src/test/java/com/campuscore/campucore_backend/CampucoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campuscore.campucore_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampucoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
